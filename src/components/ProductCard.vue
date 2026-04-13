3c851d599fcc363effdaa1d08caa6d41df7a42bf
Ayelén Silva <ayelen.sl@gmail.com>
2026-04-12 20:18:43 -0400

    Lección 2: Integración Axios-API | Mostrar douctos dinámicamente | Implementar estados de carga | filtro por categoría

--- src/components/ProductCard.vue
+++ src/components/ProductCard.vue
@@ -14,16 +14,23 @@ onMounted(() => {
 </script>
 
 <template>
-  <div class="product-card">
-    <h3>{{ product.name }}</h3>
-    <p>{{ product.description }}</p>
-    <span>{{ product.price }}</span>
+  <div>
+    <div class="product-card ">
+      <img :src="product.image" alt="Product Image" width="100" />
+      <h3>{{ product.title }}</h3>
+      <span>${{ product.price }}</span>
+    </div>
   </div>
 </template>
 
 <style scoped>
 .product-card {
-  border: 1px solid #ccc;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  min-width: 25%;
+  max-width: 50%;
+  border: 1px solid #7dbdc0;
   padding: 1rem;
   margin: 5rem;
 }
--- src/components/ProductList.vue
+++ src/components/ProductList.vue
@@ -1,32 +1,86 @@
 <script setup>
 import ProductCard from './ProductCard.vue';
+import { onMounted, ref } from 'vue';
+import axios from 'axios';
+import { computed } from 'vue';
 
-const products = [
-  {
-    id: 1,
-    name: 'Laptop',
-    description: 'Equipo portátil de alta gama',
-    price: '$1.200.000'
-  },
-  {
-    id: 2,
-    name: 'Smartphone',
-    description: 'Teléfono inteligente con pantalla OLED',
-    price: '$800.000'
-  },
-  {
-    id: 3,
-    name: 'Tablet',
-    description: 'Dispositivo táctil para entretenimiento y trabajo',
-    price: '$600.000'
+const products = ref([]);
+const loading = ref(true);
+const error = ref(null);
+
+const selectedCategory = ref(''); //muestra todo
+
+const filteredProducts = computed(() => {
+  if (!selectedCategory.value) {
+    return products.value;
   }
-]
+  return products.value.filter(product => product.category === selectedCategory.value);
+});
+
+onMounted(async () => {
+  try {
+    const response = await axios.get('http://fakestoreapi.com/products')
+    products.value = response.data;
+  } catch (err) {
+    error.value = 'Failed to load products. Please try again later.';
+    console.error(err);
+  } finally {
+    loading.value = false;
+  }
+});
 </script>
 
 <template>
   <div>
-    <ProductCard v-for="product in products" :key="product.id" :product="product" />
+    <div class="selection">
+      <label for="category">Filtrar por categoría:</label>
+      <select id="category" v-model="selectedCategory">
+        <option value="">Todas</option>
+        <option value="electronics">Electrónica</option>
+        <option value="jewelery">Joyería</option>
+        <option value="men's clothing">Ropa de hombre</option>
+        <option value="women's clothing">Ropa de mujer</option>
+      </select>
+    </div>
+
+    <p v-if="loading">Cargando productos...</p>
+    <p v-else-if="error">{{ error }}</p>
+    <!-- empty -->
+    <p v-else-if="filteredProducts.length === 0">No hay productos disponibles</p>
+
+    <ProductCard v-for="product in filteredProducts" :key="product.id" :product="product" />
   </div>
 </template>
 
-<style scoped></style>
+<style scoped>
+* {
+  box-sizing: border-box;
+  font-family: system-ui, -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;
+}
+
+.selection {
+  margin: 1rem;
+  display: flex;
+  justify-content: end;
+}
+
+label {
+  margin-right: 0.5rem;
+  font-size: 1rem;
+  color: #333;
+  align-items: center;
+}
+
+select {
+  font-size: 0.75rem;
+  padding: 0.25rem;
+  color: #333;
+  border: 1px darkcyan solid;
+  border-radius: 4px;
+}
+
+option {
+  padding: 0.25rem;
+  font-size: 0.75rem;
+}
+</style>
